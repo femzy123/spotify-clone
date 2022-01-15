c466b97c215b18acb3ce79b042b6f2dae55a9d69
femzy123 <femzy123@yahoo.com>
2022-01-15 09:27:24 +0100

    First Commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  HeartIcon
+} from "@heroicons/react/outline";
+
+interface Props {}
+
+const menus = [
+  {
+    name: "Home",
+    icon: <HomeIcon className="h-5 w-5" />,
+  },
+  {
+    name: "Search",
+    icon: <SearchIcon className="h-5 w-5" />,
+  },
+  {
+    name: "Library",
+    icon: <LibraryIcon className="h-5 w-5" />,
+  },
+];
+
+const Sidebar = (props: Props) => {
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        {menus.map((menu, i) => (
+          <button
+            key={i}
+            className="flex items-center space-x-2 hover:text-white"
+          >
+            {menu.icon}
+            <p>{menu.name}</p>
+          </button>
+        ))}
+        <hr className="border-t-[0.10px] border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your Episodes</p>
+        </button>
+        
+        <hr className="border-t-[0.10px] border-gray-900" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
