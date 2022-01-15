57d34e2c5e3563e314b3c770d46574fc6bfaa54a
femzy123 <femzy123@yahoo.com>
2022-01-15 23:42:32 +0100

    Header UI created

---
+++ components/Center.tsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import { ChevronDownIcon, UserCircleIcon } from '@heroicons/react/outline';
+import { shuffle } from 'lodash';
+
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+  "from-orange-500",
+  "from-red-500",
+  "from-teal-500",
+]
+
+const Center = () => {
+  const {data: session} = useSession();
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop());
+  }, []);
+
+  return (
+    <div className="flex-grow">
+      <header className='absolute top-5 right-8'>
+        <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+          {/* <img src={session?.user?.image} alt="" /> */}
+          <UserCircleIcon className="w-10 h-10" />
+          <h2>{session?.user?.name}</h2>
+          <ChevronDownIcon className="w-5 h-5" />
+        </div>
+      </header>
+
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
+        hello
+      </section>
+    </div>
+  );
+}
+
+export default Center
